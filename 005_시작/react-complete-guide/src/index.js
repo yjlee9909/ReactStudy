7453c45e1420c641e1c5ff9af54071c6e4b7329c
yjlee9909 <yjlee9909@naver.com>
2022-12-02 00:08:19 +0900

    add: 리액트 구조 살펴보기

--- 005_시작/react-complete-guide/src/index.js
+++ 005_시작/react-complete-guide/src/index.js
@@ -6,6 +6,3 @@ import App from "./App";
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(<App />);
-
-// npx create-react-app my-app --template basic-react
-// npx create-react-app react-complete-guide --template basic-react
