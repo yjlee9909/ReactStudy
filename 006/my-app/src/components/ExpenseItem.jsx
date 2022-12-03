6399fd903f760b8f3c3dbe1c5cce8fd921179dfd
yjlee9909 <yjlee9909@naver.com>
2022-12-04 01:08:29 +0900

    컴포넌트 분할

--- 006/my-app/src/components/ExpenseItem.jsx
+++ 006/my-app/src/components/ExpenseItem.jsx
@@ -1,10 +1,10 @@
+import ExpenseDate from "./ExpenseDate";
 import "./expenseItem.css";
-const ExpenseItem = (props) => {
-    const expenseDate = new Date();
 
+const ExpenseItem = (props) => {
     return (
         <div className="expense-item">
-            <div>{props.date.toISOString()}</div>
+            <ExpenseDate date={props.date} />
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
