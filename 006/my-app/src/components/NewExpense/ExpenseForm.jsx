71f8e95564ea9bff01156fbec03a5745cf2e6789
yjlee9909 <yjlee9909@naver.com>
2022-12-07 00:58:22 +0900

    add: useState 끌어 올리기

--- 006/my-app/src/components/NewExpense/ExpenseForm.jsx
+++ 006/my-app/src/components/NewExpense/ExpenseForm.jsx
@@ -1,21 +1,43 @@
 import { useState } from "react";
 import "./ExpenseForm.css";
 const ExpenseForm = () => {
-  const [enteredTitle, setEnteredTitle] = useState("");
-  const [enteredAmount, setEnteredAmount] = useState("");
-  const [enteredDate, setEnteredDate] = useState("");
+  //   const [enteredTitle, setEnteredTitle] = useState("");
+  //   const [enteredAmount, setEnteredAmount] = useState("");
+  //   const [enteredDate, setEnteredDate] = useState("");
+  //
+
+  // lifting-state-up
+  // useState를 한 번 선언하여 관리하기 (객체형)
+  const [userInput, setUserInput] = useState({
+    entertedTitle: "",
+    enteredAmount: "",
+    enteredDate: "",
+  });
 
   const titleChangeHandler = (event) => {
     console.log(event.target.value);
-    setEnteredTitle(event.target.value);
+    // setEnteredTitle(event.target.value);
+    // useState는 이전의 상태를 버리기 때문에 스프레드 문법을 사용하여 덮어쓰는 방식
+    setUserInput({
+      ...userInput,
+      entertedTitle: event.target.value,
+    });
   };
   const amountChangeHandler = (event) => {
     console.log(event.target.value);
-    setEnteredAmount(event.target.value);
+    // setEnteredAmount(event.target.value);
+    setUserInput({
+      ...userInput,
+      enteredAmount: event.target.value,
+    });
   };
   const dateChangeHandler = (event) => {
     console.log(event.target.value);
-    setEnteredDate(event.target.value);
+    // setEnteredDate(event.target.value);
+    setUserInput({
+      ...userInput,
+      enteredDate: event.target.value,
+    });
   };
   return (
     <form>
