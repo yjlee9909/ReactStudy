4c66adc19b3911a5f21ffa28f9da743a9db4913f
yjlee9909 <yjlee9909@naver.com>
2022-12-09 00:27:58 +0900

    add: 조건식 & 삼항식 & 변수 할당

--- 006/my-app/src/components/Expenses/Expenses.jsx
+++ 006/my-app/src/components/Expenses/Expenses.jsx
@@ -15,6 +15,17 @@ const Expenses = (props) => {
   const filteredExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
+  let expensesContent = <p>No expenses found.</p>;
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
   return (
     <div>
       <Card className="expenses">
@@ -22,14 +33,17 @@ const Expenses = (props) => {
           onChangeFilter={filterChangeHandler}
           selected={filteredYear}
         />
-        {filteredExpenses.map((expense) => (
+        {/* {filteredExpenses.length === 0 && <p>No expenses found.</p>} */}
+        {/* {filteredExpenses.map((expense) => (
           <ExpenseItem
             key={expense.id}
             title={expense.title}
             amount={expense.amount}
             date={expense.date}
           />
-        ))}
+        ))} */}
+
+        {expensesContent}
       </Card>
     </div>
   );
