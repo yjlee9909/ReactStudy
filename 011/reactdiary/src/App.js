3c24e8a6712524c0145cd8dbb68b662ff1cf60aa
yjlee9909 <yjlee9909@naver.com>
2023-08-29 02:45:45 +0900

    add: map 이용한 리스트 배열 작성

--- 011/reactdiary/src/App.js
+++ 011/reactdiary/src/App.js
@@ -1,10 +1,36 @@
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
+
+const dummyList = [
+  {
+    id: 1,
+    author: "작성자1",
+    content: "hello1",
+    emotion: 1,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    author: "작성자2",
+    content: "hello2",
+    emotion: 2,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    author: "작성자3",
+    content: "hello3",
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <DiaryEditor/>
+      <DiaryEditor />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 }
--- 011/reactdiary/src/DiaryEditor.jsx
+++ 011/reactdiary/src/DiaryEditor.jsx
@@ -1,72 +1,69 @@
-import { useRef,useState } from "react"
+import { useRef, useState } from "react";
 
+const DiaryEditor = () => {
+  // DOM 요소 선택
+  const authorInput = useRef();
+  const contentInput = useRef();
+  // state 묶어주기
+  const [state, setState] = useState({
+    author: "",
+    content: "",
+    emotion: 1,
+  });
 
+  // const [author, setAuthor] = useState("");
+  // const [content, setContent] = useState("");
 
-const DiaryEditor = ()=> {
-    // DOM 요소 선택
-    const authorInput = useRef();
-    const contentInput = useRef();
-    // state 묶어주기
-    const [state, setState] = useState({
-        author: "",
-        content: "",
-        emotion: 1,
-    })
+  const handleChangeState = (e) => {
+    // console.log(e.target.name)
+    // console.log(e.target.value)
 
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-    // const [author, setAuthor] = useState("");
-    // const [content, setContent] = useState("");
-
-    const handleChangeState = (e) => {
-        // console.log(e.target.name)
-        // console.log(e.target.value)
-
-        setState({
-            ...state,
-            [e.target.name]: e.target.value,
-        })
+  const handleSubmit = () => {
+    if (state.author.length < 1) {
+      // alert('작성자는 최소 1글자 이상 입력해주세요');
+      // focus()
+      authorInput.current.focus();
+      // 조건이 만족하지 못한다면 return 더이상 실행x
+      return;
     }
-
-    const handleSubmit = () => {
-        if(state.author.length < 1) {
-            // alert('작성자는 최소 1글자 이상 입력해주세요');
-            // focus() 
-            authorInput.current.focus();
-            // 조건이 만족하지 못한다면 return 더이상 실행x
-            return;
-        }
-        if(state.content.length < 5) {
-            // alert('일기 본문은 최소 5글자 이상 입력해주세요');
-            // focus
-            contentInput.current.focus();
-            return;
-        }
-        alert("저장 성공")
+    if (state.content.length < 5) {
+      // alert('일기 본문은 최소 5글자 이상 입력해주세요');
+      // focus
+      contentInput.current.focus();
+      return;
     }
+    alert("저장 성공");
+  };
 
-    return (
-        <div className="DiaryEditor">
-            <h2>오늘의 일기</h2>
-            <div>
-                <input ref={authorInput} name="author" value={state.author} onChange={handleChangeState}/>
-            </div>
-            <div>
-                <textarea ref={contentInput} name="content" value={state.content} onChange={handleChangeState}/>
-            </div>
-            <div>
-                <span>오늘의 감정점수 : </span>
-                <select name="emotion" value={state.emotion} onChange={handleChangeState}>
-                    <option value={1}>1</option>
-                    <option value={2}>2</option>
-                    <option value={3}>3</option>
-                    <option value={4}>4</option>
-                    <option value={5}>5</option>
-                </select>
-            </div>
-            <div>
-                <button onClick={handleSubmit}>일기 저장하기</button>
-            </div>
-        </div>
-    )
-}
-export default DiaryEditor
+  return (
+    <div className="DiaryEditor">
+      <h2>오늘의 일기</h2>
+      <div>
+        <input ref={authorInput} name="author" value={state.author} onChange={handleChangeState} />
+      </div>
+      <div>
+        <textarea ref={contentInput} name="content" value={state.content} onChange={handleChangeState} />
+      </div>
+      <div>
+        <span>오늘의 감정점수 : </span>
+        <select name="emotion" value={state.emotion} onChange={handleChangeState}>
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5}>5</option>
+        </select>
+      </div>
+      <div>
+        <button onClick={handleSubmit}>일기 저장하기</button>
+      </div>
+    </div>
+  );
+};
+export default DiaryEditor;
