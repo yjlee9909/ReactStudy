587adac88ce0facebf0cd808d9ef1daae67ced04
yjlee9909 <yjlee9909@naver.com>
2023-12-21 15:37:08 +0900

    useReducer() 훅 사용

--- 014/login_effect/src/components/Login/Login.js
+++ 014/login_effect/src/components/Login/Login.js
@@ -1,41 +1,70 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useReducer } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
 
+// useReducer의 첫번째 인자 (가장 최신의 state를 받아 옴)
+const emailReducer = (state, action) => {
+  if (action.type === "USER_INPUT") {
+    // useReducer의 emailState 형태는 {value:'',isvalid:undefined}
+    return { value: action.val, isValid: action.val.includes("@") };
+  }
+  if (action.type === "INPUT_BLUR") {
+    return { value: state.value, isValid: state.value.includes("@") };
+  }
+  return { value: "", isValid: false };
+};
+
 const Login = (props) => {
-  const [enteredEmail, setEnteredEmail] = useState("");
-  const [emailIsValid, setEmailIsValid] = useState();
+  // const [enteredEmail, setEnteredEmail] = useState("");
+  // const [emailIsValid, setEmailIsValid] = useState();
   const [enteredPassword, setEnteredPassword] = useState("");
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
-  // [] 안의 조건일 경우에 재렌더링
-  useEffect(() => {
-    const identifier = setTimeout(() => {
-      console.log("유효성 체크!");
-      setFormIsValid(
-        enteredEmail.includes("@") && enteredPassword.trim().length > 6
-      );
-    }, 500); // 500밀리초 후에만 이 작업을 수행
+  // useReducer 사용
+  // const [state, dispatch] = useReducer(reducer, initialState); 형태
+  // emailReducer 함수는 dispatchEmail이 호출될 때 자동으로 새로운 state 반환
+  const [emailState, dispatchEmail] = useReducer(emailReducer, {
+    value: "",
+    isValid: undefined,
+  }); // 2번째 인자 초기 state
 
-    return () => {
-      console.log("클린업");
-      clearTimeout(identifier); // 빠르게 입력시 "클린업"*n번, "유효성 체크"는 마지막 1번만 출력
-    }; // 반환 가능 (클린업 함수), 첫 번째 사이드이펙트가 실행되기 전에는 실행x
-  }, [enteredEmail, enteredPassword]);
+  // [] 안의 조건일 경우에 재렌더링
+  // useEffect(() => {
+  //   const identifier = setTimeout(() => {
+  //     console.log("유효성 체크!");
+  //     setFormIsValid(
+  //       enteredEmail.includes("@") && enteredPassword.trim().length > 6
+  //     );
+  //   }, 500); // 500밀리초 후에만 이 작업을 수행
+
+  //   return () => {
+  //     console.log("클린업");
+  //     clearTimeout(identifier); // 빠르게 입력시 "클린업"*n번, "유효성 체크"는 마지막 1번만 출력
+  //   }; // 반환 가능 (클린업 함수), 첫 번째 사이드이펙트가 실행되기 전에는 실행x
+  // }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
-    setEnteredEmail(event.target.value);
+    dispatchEmail({
+      type: "USER_INPUT",
+      val: event.target.value,
+    });
+    setFormIsValid(
+      event.target.value.includes("@") && enteredPassword.trim().length > 6
+    );
   };
 
   const passwordChangeHandler = (event) => {
     setEnteredPassword(event.target.value);
+    setFormIsValid(emailState.isValid && event.target.value.trim().length > 6);
   };
 
+  // 다른 state와 연관 (오류 발생할 수도 있음) => useReducer 사용 권장
   const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes("@"));
+    // setEmailIsValid(emailState.isValid);
+    dispatchEmail({ type: "INPUT_BLUR" });
   };
 
   const validatePasswordHandler = () => {
@@ -44,7 +73,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(enteredEmail, enteredPassword);
+    props.onLogin(emailState.value, enteredPassword);
   };
 
   return (
@@ -52,14 +81,14 @@ const Login = (props) => {
       <form onSubmit={submitHandler}>
         <div
           className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ""
+            emailState.isValid === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="email">E-Mail</label>
           <input
             type="email"
             id="email"
-            value={enteredEmail}
+            value={emailState.value}
             onChange={emailChangeHandler}
             onBlur={validateEmailHandler}
           />
