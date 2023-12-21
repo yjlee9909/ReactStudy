762a36fb5fa6558d554a5b6edbc26320cb0ddd62
yjlee9909 <yjlee9909@naver.com>
2023-12-21 16:58:26 +0900

    useReducer & useEffect 최적화

--- 014/login_effect/src/components/Login/Login.js
+++ 014/login_effect/src/components/Login/Login.js
@@ -15,12 +15,22 @@ const emailReducer = (state, action) => {
   }
   return { value: "", isValid: false };
 };
+const passwordReducer = (state, action) => {
+  if (action.type === "USER_INPUT") {
+    // useReducer의 emailState 형태는 {value:'',isvalid:undefined}
+    return { value: action.val, isValid: action.val.trim().length > 6 };
+  }
+  if (action.type === "INPUT_BLUR") {
+    return { value: state.value, isValid: state.value.trim().length > 6 };
+  }
+  return { value: "", isValid: false };
+};
 
 const Login = (props) => {
   // const [enteredEmail, setEnteredEmail] = useState("");
   // const [emailIsValid, setEmailIsValid] = useState();
-  const [enteredPassword, setEnteredPassword] = useState("");
-  const [passwordIsValid, setPasswordIsValid] = useState();
+  // const [enteredPassword, setEnteredPassword] = useState("");
+  // const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
   // useReducer 사용
@@ -28,37 +38,45 @@ const Login = (props) => {
   // emailReducer 함수는 dispatchEmail이 호출될 때 자동으로 새로운 state 반환
   const [emailState, dispatchEmail] = useReducer(emailReducer, {
     value: "",
-    isValid: undefined,
+    isValid: null,
   }); // 2번째 인자 초기 state
 
-  // [] 안의 조건일 경우에 재렌더링
-  // useEffect(() => {
-  //   const identifier = setTimeout(() => {
-  //     console.log("유효성 체크!");
-  //     setFormIsValid(
-  //       enteredEmail.includes("@") && enteredPassword.trim().length > 6
-  //     );
-  //   }, 500); // 500밀리초 후에만 이 작업을 수행
+  const [passwordState, dispatchPassword] = useReducer(passwordReducer, {
+    value: "",
+    isValid: null,
+  });
 
-  //   return () => {
-  //     console.log("클린업");
-  //     clearTimeout(identifier); // 빠르게 입력시 "클린업"*n번, "유효성 체크"는 마지막 1번만 출력
-  //   }; // 반환 가능 (클린업 함수), 첫 번째 사이드이펙트가 실행되기 전에는 실행x
-  // }, [enteredEmail, enteredPassword]);
+  // 별칭 할당
+  const { isValid: emailIsValid } = emailState;
+  const { isValid: passwordIsValid } = passwordState;
+
+  // [] 안의 조건일 경우에 재렌더링
+  useEffect(() => {
+    const identifier = setTimeout(() => {
+      console.log("유효성 체크!");
+      setFormIsValid(emailIsValid && passwordIsValid);
+    }, 500); // 500밀리초 후에만 이 작업을 수행
+
+    return () => {
+      console.log("클린업");
+      clearTimeout(identifier); // 빠르게 입력시 "클린업"*n번, "유효성 체크"는 마지막 1번만 출력
+    }; // 반환 가능 (클린업 함수), 첫 번째 사이드이펙트가 실행되기 전에는 실행x
+  }, [emailIsValid, passwordIsValid]);
 
   const emailChangeHandler = (event) => {
     dispatchEmail({
       type: "USER_INPUT",
       val: event.target.value,
     });
-    setFormIsValid(
-      event.target.value.includes("@") && enteredPassword.trim().length > 6
-    );
+    // setFormIsValid(event.target.value.includes("@") && passwordState.isValid);
   };
 
   const passwordChangeHandler = (event) => {
-    setEnteredPassword(event.target.value);
-    setFormIsValid(emailState.isValid && event.target.value.trim().length > 6);
+    dispatchPassword({
+      type: "USER_INPUT",
+      val: event.target.value,
+    });
+    // setFormIsValid(emailState.isValid && event.target.value.trim().length > 6);
   };
 
   // 다른 state와 연관 (오류 발생할 수도 있음) => useReducer 사용 권장
@@ -68,12 +86,12 @@ const Login = (props) => {
   };
 
   const validatePasswordHandler = () => {
-    setPasswordIsValid(enteredPassword.trim().length > 6);
+    dispatchPassword({ type: "INPUT_BLUR" });
   };
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, enteredPassword);
+    props.onLogin(emailState.value, passwordState.value);
   };
 
   return (
@@ -95,14 +113,14 @@ const Login = (props) => {
         </div>
         <div
           className={`${classes.control} ${
-            passwordIsValid === false ? classes.invalid : ""
+            passwordState.isValid === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="password">Password</label>
           <input
             type="password"
             id="password"
-            value={enteredPassword}
+            value={passwordState.value}
             onChange={passwordChangeHandler}
             onBlur={validatePasswordHandler}
           />
