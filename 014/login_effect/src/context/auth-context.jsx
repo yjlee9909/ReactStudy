55c3280063f4afa7928a78ec07078a455bd09658
yjlee9909 <yjlee9909@naver.com>
2023-12-28 14:36:36 +0900

    컨텍스트

--- 014/login_effect/src/context/auth-context.jsx
+++ 014/login_effect/src/context/auth-context.jsx
@@ -1,6 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
 const AuthContext = React.createContext({
   isLoggedIn: false,
+  onLogout: () => {},
+  onLogin: (email, password) => {},
 });
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
+    setIsLoggedIn(false);
+  };
+  const loginHandler = () => {
+    localStorage.setItem("isLoggedIn", "1");
+    setIsLoggedIn(true);
+  };
 
+  // 앱이 실행될 때 한 번만
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+    if (storedUserLoggedInInformation === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        onLogin: loginHandler,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
 export default AuthContext;
